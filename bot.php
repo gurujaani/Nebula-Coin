c90d57db7d0dd1a700b961bd9be9db27a84b21e7
gurujaani <guru7298@yahoo.com>
2024-10-05 18:13:13 +0530

    Update bot.php

--- bot.php
+++ bot.php
@@ -10,7 +10,7 @@ if($message == "/start") {
 }
 
 function sendMessage($chat_id, $message) {
-    $apiToken = "7930320812:AAH264MiuNQHIF7K18lb7Cf7R2eZlfKK1fw";
+    $apiToken = "7896884637:AAHXGmfM7J34Vfn0c0dyJVMvbwJlKe1OC-M";
     $url = "https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$chat_id&text=".urlencode($message);
     file_get_contents($url);
 }
